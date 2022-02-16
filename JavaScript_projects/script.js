848ba7750cdc2e4222d5fdfaa15d9ea29005b533
RandyLaderoute <81232589+RandyLaderoute@users.noreply.github.com>
2022-02-16 04:45:49 -0800

    update

---
+++ JavaScript_projects/script.js
@@ -0,0 +1,10 @@
+window.alert("this is my alert");
+
+document.write("this is my string".fontcolor("red"));
+
+
+var name = "Randy"
+var age = "42"
+console.log("my name is" + name + "and i am" + age + "years old");
+
+
