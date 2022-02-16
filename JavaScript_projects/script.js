00605c40d4fcd2aadf47d72e5894ce792f241979
RandyLaderoute <81232589+RandyLaderoute@users.noreply.github.com>
2022-02-16 15:24:21 -0800

    updated operators

--- JavaScript_projects/script.js
+++ JavaScript_projects/script.js
@@ -1,15 +1,4 @@
-var str = new String("here is a changed color text");
-document.write(str.fontcolor('red'));
-
-
-
-
-function myFunction() {
-    var text1="you clicked me!";
-    var text2=" way to go";
-    var text3= text1 + text2;
-    document.getElementById("btn").innerHTML
-    =text3;
-}
-
+var text1 = document.getElementById("string1");
+var text2 = document.getElementById("string2");
+console.log(string1 + string2);
 
